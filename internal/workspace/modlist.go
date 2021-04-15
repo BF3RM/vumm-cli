72acb96a3ef98527aba15c99abc721a943d5ef8c
paulhobbel <itsdizzy40@gmail.com>
2021-04-15 22:25:19 +0200

    fix: close ModList.txt file handle after reading and save as 0x666 instead of 0x644

--- internal/workspace/modlist.go
+++ internal/workspace/modlist.go
@@ -31,6 +31,7 @@ func TryLoadModList(dir string) (*ModList, error) {
 
 		return nil, err
 	}
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -101,7 +102,7 @@ func (m ModList) isModEnabled(mod string) (bool, int) {
 func (m *ModList) Save() error {
 	log.WithField("file", "ModList.txt").Debugf("saving ModList.txt")
 
-	file, err := os.OpenFile(filepath.Join(m.dir, "ModList.txt"), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0x644)
+	file, err := os.OpenFile(filepath.Join(m.dir, "ModList.txt"), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0x666)
 	if err != nil {
 		return err
 	}
