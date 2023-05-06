a00ab7a25e7cf4e58ad04b7275d2ecf15ea056e4
paulhobbel <itsdizzy40@gmail.com>
2023-05-07 00:26:43 +0200

    feat(installer): add installer cargo

---
+++ examples/client_get_mod.rs
@@ -0,0 +1,25 @@
+use vumm_api::ClientError;
+
+#[tokio::main]
+async fn main() {
+    let client = vumm_api::Client::new();
+
+    let mod_name = String::from("mapeditor");
+    let mod_version = String::from("0.2.0");
+
+    let mod_response = client.mods().get_version(mod_name, mod_version).await;
+
+    match mod_response {
+        Ok(mod_) => {
+            println!("Mod: {:?}", mod_);
+        }
+        Err(e) => match e {
+            ClientError::StatusCode(response) => {
+                println!("Error: {}", response.status());
+            }
+            ClientError::Internal(e) => {
+                println!("Error: {}", e);
+            }
+        },
+    }
+}
